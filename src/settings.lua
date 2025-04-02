8cce996edc99bf1f82aa8a1b1cfea8301dda4b42
DirkDigglerz <liamsdargie@gmail.com>
2025-04-02 20:59:31 +0100

    ...

--- src/settings.lua
+++ src/settings.lua
@@ -1,5 +1,6 @@
 local autodetected = require 'src.autodetect'
-return {
+
+local settings = {
   primaryColor    = GetConvar('dirk_lib:primaryColor', 'dirk'),
   primaryShade    = GetConvarInt('dirk_lib:primaryShade', 9),
   customTheme     = json.decode(GetConvar('dirk_lib:customTheme', json.encode({
@@ -22,22 +23,6 @@ return {
   serverName      = GetConvar('dirk_lib:serverName', 'DirkRP'),
   logo              = GetConvar('dirk_lib:logo', 'https://via.placeholder.com/150'),
 
-  framework         = GetConvar('dirk_lib:framework', autodetected.framework),
-  inventory         = GetConvar('dirk_lib:inventory', autodetected.inventory),
-  itemImgPath       = GetConvar('dirk_lib:itemImgPath', autodetected.itemImgPath),
-  target            = GetConvar('dirk_lib:target', autodetected.target),
-  interact          = GetConvar('dirk_lib:interact', autodetected.interact),
-  time              = GetConvar('dirk_lib:time', autodetected.time),
-  phone             = GetConvar('dirk_lib:phone', autodetected.phone),
-
-  keys            = GetConvar('dirk_lib:keys', autodetected.keys),
-  garage          = GetConvar('dirk_lib:garage', autodetected.garage),
-  fuel            = GetConvar('dirk_lib:fuel', autodetected.fuel),
-  
-  ambulance       = GetConvar('dirk_lib:ambulance', autodetected.ambulance),
-  prison          = GetConvar('dirk_lib:prison', autodetected.prison),
-  dispatch        = GetConvar('dirk_lib:dispatch', autodetected.dispatch),
-
   --## Menus/Progress etc  
   notify          = GetConvar('dirk_lib:notify', 'dirk_lib'),
   notifyPosition  = GetConvar('dirk_lib:notifyPosition', 'top-right'),
@@ -56,9 +41,7 @@ return {
   dialog            = GetConvar('dirk_lib:dialog', 'dirk_lib'),
   dialogClickSounds = GetConvar('dirk_lib:dialogClickSounds', 'true') == 'true',
   dialogHoverSounds = GetConvar('dirk_lib:dialogHoverSounds', 'true') == 'true',
-
-
-
+  
   -- GROUPS 
   groups = {
     maxMembers        = GetConvarInt('dirk_groups:maxMembers', 5),
@@ -67,4 +50,14 @@ return {
     maxLogOffTime     = GetConvarInt('dirk_groups:maxLogOffTime', 5), -- minutes before you are autokicked for being offline
   },
 }
-  
+
+
+---@todo add implementation for renamed resources 
+-- local renamed = json.decode(GetConvar('dirk_lib:renamedResources', json.encode({})))
+
+for system,resource in pairs(autodetected) do 
+  -- Allows for convar overriding of used resource. 
+  settings[system] = GetConvar(('dirk_lib:%s'):format(system), resource)
+end
+
+return settings
