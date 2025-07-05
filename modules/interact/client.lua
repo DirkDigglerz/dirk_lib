7e2009acf7dd6abc092d75dbdeb813dcbb34f6ac
DirkDigglerz <liamsdargie@gmail.com>
2025-07-05 14:02:37 +0100

    feat: added support for https://github.com/darktrovx/interact (Suggested by ma141)

--- modules/interact/client.lua
+++ modules/interact/client.lua
@@ -29,8 +29,20 @@ lib.interact = {
         exports.sleepless_interact:addLocalEntity(interact_data)
       end
         
-    elseif settings.interact == 'redm-uiprompt' then 
-
+    elseif settings.interact == 'interact' then
+      local export = data.networked and 'AddEntityInteraction' or 'AddLocalEntityInteraction' 
+      exports.interact:[export]({
+        netId = data.networked and entity or nil, -- needed for networked entities
+        entity = not data.networked and entity or nil, -- needed for local entities
+        id = data.id or ('entity_%s'):format(entity), -- unique id for the interaction
+        distance = data.distance or 1.5, -- distance at which the interaction is active
+        interactDst = data.renderDistance or 10.0, -- distance at which the interaction is rendered
+        ignoreLos = false, -- optional ignores line of sight
+        offset = data.offset or vector3(0.0, 0.0, 0.0), -- optional offset from the entity
+        bone = data.bone or nil, -- optional bone to attach the interaction to
+        groups = data.groups or nil, -- optional groups to restrict the interaction to
+        options = parseOptions(data.options), -- options for the interaction
+      })
     end 
   end,
 
@@ -47,7 +59,7 @@ lib.interact = {
         offset         = data.offset,
       }
       exports.sleepless_interact:addGlobalModel(interact_data)
-    elseif settings.interact == 'marker' then 
+    elseif settings.interact == 'interact' then 
 
     end 
   end, 
@@ -66,8 +78,18 @@ lib.interact = {
       }
       exports.sleepless_interact:addGlobalVehicle(options)
       return id 
-    elseif settings.ineract == 'marker' then 
-
+    elseif settings.ineract == 'interact' then 
+      exports.interact:AddGlobalVehicleInteraction({
+        id = id,
+        model = data.model,
+        distance = data.distance or 1.5,
+        interactDst = data.renderDistance or 10.0,
+        ignoreLos = false, -- optional ignores line of sight
+        offset = data.offset or vector3(0.0, 0.0, 0.0), -- optional offset from the entity
+        bone = data.bone or nil, -- optional bone to attach the interaction to
+        groups = data.groups or nil, -- optional groups to restrict the interaction to
+        options = parseOptions(data.options), -- options for the interaction
+      })
     end 
   end,
 
@@ -83,8 +105,16 @@ lib.interact = {
         offset         = data.offset,
       }
       exports.sleepless_interact:addCoords(interact_data)
-    elseif settings.interact == 'marker' then 
-
+    elseif settings.interact == 'interact' then 
+      exports.interact:AddInteraction({
+        id = data.id or ('coords_%s'):format(data.pos), 
+        coords = vector3(data.pos.x, data.pos.y, data.pos.z), 
+        distance = data.distance or 1.5,
+        interactDst = data.renderDistance or 10.0,
+        ignoreLos = false, -- optional ignores line of sight
+        groups = data.groups or nil, -- optional groups to restrict the interaction to
+        options = parseOptions(data.options), -- options for the interaction
+      })
     end
   end,
 
@@ -99,8 +129,16 @@ lib.interact = {
         offset         = data.offset,
       }
       exports.sleepless_interact:addGlobalPlayer(interact_data)
-    elseif settings.interact == 'marker' then 
-
+    elseif settings.interact == 'interact' then 
+      exports.interact:AddGlobalPlayerInteraction({
+        id = data.id or ('player_%s'):format(math.random(1, 1000000)), 
+        distance = data.distance or 1.5,
+        interactDst = data.renderDistance or 10.0,
+        ignoreLos = false, -- optional ignores line of sight
+        offset = data.offset or vector3(0.0, 0.0, 0.0), -- optional offset from the entity
+        groups = data.groups or nil, -- optional groups to restrict the interaction to
+        options = parseOptions(data.options), -- options for the interaction
+      })
     end 
   end,
 
@@ -116,8 +154,16 @@ lib.interact = {
       }
   
       exports.sleepless_interact:addGlobalPed(interact_data)
-    elseif settings.interact == 'marker' then 
-
+    elseif settings.interact == 'interact' then 
+      exports.interact:addGlobalPlayerInteraction({
+        id = data.id or ('ped_%s'):format(math.random(1, 1000000)), 
+        distance = data.distance or 1.5,
+        interactDst = data.renderDistance or 10.0,
+        ignoreLos = false, -- optional ignores line of sight
+        offset = data.offset or vector3(0.0, 0.0, 0.0), -- optional offset from the entity
+        groups = data.groups or nil, -- optional groups to restrict the interaction to
+        options = parseOptions(data.options), -- options for the interaction
+      })
     end 
   end,
 
@@ -125,32 +171,26 @@ lib.interact = {
   removeById = function(id)
     if settings.interact == 'sleepless_interact' then
       exports.sleepless_interact:removeById(id)
-    elseif settings.interact == 'marker' then 
-
+    elseif settings.interact == 'interact' then 
+      exports.interact:RemoveInteraction(id)
     end 
   end,
 
   removeEntity = function(entity)
     if settings.interact == 'sleepless_interact' then
       exports.sleepless_interact:removeEntity(entity)
-    elseif settings.interact == 'marker' then 
-
     end 
   end,
 
   removeGlobalModel = function(model)
     if settings.interact == 'sleepless_interact' then
       exports.sleepless_interact:removeGlobalModel(model)
-    elseif settings.interact == 'marker' then 
-
     end 
   end,
 
   removeGlobalPlayer = function(player)
     if settings.interact == 'sleepless_interact' then
       exports.sleepless_interact:removeGlobalPlayer(player)
-    elseif settings.interact == 'marker' then 
-
     end 
   end,
 
