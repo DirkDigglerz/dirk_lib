5a78a6141336aefa79dfa589a62862c9bc74b9e2
DirkDigglerz <liamsdargie@gmail.com>
2025-07-11 15:33:09 +0100

    fix: illenium-appearance not setting correct model (dirk_multichar)

--- bridge/clothing/illenium-appearance/client.lua
+++ bridge/clothing/illenium-appearance/client.lua
@@ -2,7 +2,7 @@ return {
   setSkin = function(ped, skin)
     local playerPed = ped or cache.ped
     if not skin then return end
-    exports[lib.settings.clothing]:setPedAppearance(ped, skin)
+    exports[lib.settings.clothing]:setPedAppearance(playerPed, skin)
   end,
 
   openCustomisation = function(categories)
